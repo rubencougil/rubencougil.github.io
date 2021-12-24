d12d8fdc5a7f76d43907cc2787f23ff27ea5c83a
Rubén Cougil <rubencougil@gmail.com>
2021-12-24 18:28:26 +0100

    itsawonderfulworld

---
+++ assets/itsawonderfulworld/js/calculator.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function(){  
+    document.addEventListener('input', function (evt) {
+        calculateScore();
+    });
+});
+
+function calculateScore() {
+    
+    var resultInput = document.getElementById("resultInput")
+    resultInput.value = 0;
+
+    var gross = parseInt(document.getElementById("grossVictoryPointsInput").value || 0);
+    var science = parseInt(document.getElementById("scienceVictoryPointsInput").value || 0);
+    var scienceMult = parseInt(document.getElementById("scienceMultiplierInput").value || 0);
+    var energy = parseInt(document.getElementById("energyVictoryPointsInput").value || 0);
+    var energyMult = parseInt(document.getElementById("energyMultiplierInput").value || 0);
+    var gold = parseInt(document.getElementById("goldVictoryPointsInput").value || 0);
+    var goldMult = parseInt(document.getElementById("goldMultiplierInput").value || 0);
+    var exploration = parseInt(document.getElementById("explorationVictoryPointsInput").value || 0);
+    var explorationMult = parseInt(document.getElementById("explorationMultiplierInput").value || 0);
+    var generals = parseInt(document.getElementById("generalsVictoryPointsInput").value || 0);
+    var generalsMult = parseInt(document.getElementById("generalsMultiplierInput").value || 0);
+    var financiers = parseInt(document.getElementById("financiersVictoryPointsInput").value || 0);
+    var financiersMult = parseInt(document.getElementById("financiersMultiplierInput").value || 0);
+
+    console.log(gross, science, scienceMult);
+    var result = (gross 
+        + (science * scienceMult) 
+        + (energy * energyMult) 
+        + (gold * goldMult) 
+        + (exploration * explorationMult)
+        + (generalsMult > 0) ? generals * (generalsMult + 1) : generals
+        + (financiersMult > 0) ? financiers * (financiersMult + 1) : financiers
+    );
+
+    resultInput.value = result;
+}
